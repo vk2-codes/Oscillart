7e4fe699744daaddda0f3c714eeb8d20b65844ea
vk2-codes <vaibhavi.kode2@gmail.com>
2025-06-11 18:52:55 -0400

    Finished css

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
 const input = document.getElementById('input');
-// create web audio api elements
+const color_picker = document.getElementById('color1');
+const color_picker2 = document.getElementById('color2');
+const vol_slider = document.getElemntById('vol-slider');
+const recording_toggle = document.getElementById('record');
+
+var interval = null;
+var reset = false;
+
+var timpernote = 0;
+var length = 0;
+
+//create web audio api elements
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
 
-// create oscillator node
-const oscillator = audioCtx.createOscillator();
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-oscillator.type = "sine"; 
-oscillator.start();
-gainNode.gain.value = 0;
-function frequency(pitch) {
-    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
-oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
-  
-}
-
-function handle() {
-    frequency (input.value);
-
-}
+//creat Oscillator node
