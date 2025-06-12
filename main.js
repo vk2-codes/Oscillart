e4921d7a030646b183b0b272a256f5416a59aeef
vk2-codes <vaibhavi.kode2@gmail.com>
2025-06-12 19:28:44 -0400

    Finished main.js code, but still need to fix some errors

--- main.js
+++ main.js
@@ -49,7 +49,6 @@ function frequency(pitch) {
     settting = setInterval(() => {gainNode.gain.value = vol_slider.value;}, 1);
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
     setTimeout(() => { clearInterval(setting); gainNode.gain.value = 0; }, ((timepernote)-10));
-}
 
 function handle() {
     reset = true;
@@ -64,22 +63,102 @@ function handle() {
 
     for (i = 0; i < usernotes.length; i++) {
         noteslist.push(notenames.get(usernotes.charAt(i)));
-    }
 
     let j = 0;
     repeat = setInterval(() => {
         if(j < noteslist.length) {
             frequency(parseInt(noteslist[j]));
             drawWave();
-            j++
+            j++;
         } else {
-            clearInterval(repeat)
+            clearInterval(repeat);
         }
+    }, timepernote);
+}
+
+var counter = 0;
+function drawWave() {
+    clearInterval(interval);
+    if (reset) {
+        ctx.clearreact(0, 0, width, height);
+        x = 0;
+        y = height / 2;
+        ctx.moveTo(x, y);
+        ctx.beginpath();
+    }
+        counter = 0;
+    interval = setInterval(line, 20);
+    reset = false;
+}
+
+function line() {
+    y = height/2 + ((vol_slider.value/100)*40 * Math.sin(x * 2 * Math.PI * freq * (0.5*length)))
+
+     const gradient = ctx.createLinearGradient(20, 0, 220, 0);
+         gradient.addColorStop(0, color_picker1.value);
+         gradient.addColorStop(1, color_picker2.value);
+
+    ctx.strokeStyle = gradient;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    x = x + 1;
+        }
+
+    //increase counter by 1 to show how long interval has been run
+    counter++;
+
+    if(counter > timepernote/20) {
+        clearInterval(interval);
+        }
+   }
+
+   var blob, recorder = null;
+   var chunks = [];
+
+   function startRecording() {
+    const canvasStream = canvas.captureStream(20); //frame rate of canvas
+    const audioDestination = audioCtx.createMediaStreamDestination();
+    gainNode.connect(audiodstination0);
+
+    const combinedStream = new MediaStream();
+
+    //Add in video data
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addtrack(track));
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+
+    recorder = new MediaRecorder(combinedStream, { type: 'video/webm' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'recording.webm';
+    a.click();
+    URL.revokeObjectURL(url);
+   };
+
+   recorder.start();
+}
+
+var is_recording = false;
+function toggle() {
+    is_recording = !is_recording;
+    if(is_recording){
+        recording_toggle.innerHTML = "Stop Recording";
+        startRecording();
+    }else{
+        recording_toggle.innerHTML = "Start Recording";
+        recorder.stop();
+    }
+    }
+
+
+   
+
+
+    
+    
 
 
     
         
         
-
-
 
