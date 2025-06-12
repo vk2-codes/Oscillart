bf68f29bfffc82088f632884179e171e896c7616
vk2-codes <vaibhavi.kode2@gmail.com>
2025-06-11 22:49:31 -0400

    Completing function handle

--- main.js
+++ main.js
@@ -25,5 +25,58 @@ gainNode.gain.value = 0;
 
 //define canvas variables
 var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+
+notenames = new Map();
+notenames.set("C", 261.6);
+notenames.set("C#", 277.2);
+notenames.set("D", 293.7);
+notenames.set("D#", 311.1);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("F#", 370.0);
+notenames.set("G", 392.0);
+notenames.set("G#", 415.3);
+notenames.set("A", 440.0);
+notenames.set("A#", 466.2);
+notenames.set("B", 493.9);
+
+function frequency(pitch) {
+    freq = pitch / 10000;
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    settting = setInterval(() => {gainNode.gain.value = vol_slider.value;}, 1);
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    setTimeout(() => { clearInterval(setting); gainNode.gain.value = 0; }, ((timepernote)-10));
+}
+
+function handle() {
+    reset = true;
+    audioCtx.resume();
+    gainNode.gain.value = 0;
+
+    var usernotes = String(input.value);
+    var noteslist = [];
+
+    length = usernotes.length;
+    timepernote = (6000 / length);
+
+    for (i = 0; i < usernotes.length; i++) {
+        noteslist.push(notenames.get(usernotes.charAt(i)));
+    }
+
+    let j = 0;
+    repeat = setInterval(() => {
+        if(j < noteslist.length) {
+            frequency(parseInt(noteslist[j]));
+            drawWave();
+            j++
+        } else {
+            clearInterval(repeat)
+        }
+        
+        
+
 
 
